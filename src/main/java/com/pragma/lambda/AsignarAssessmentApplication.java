3e6a9b9a3beb52663963c2fdde352df13568b8f8
Juan.Ruiz <juan.ruiz.22.03.00@gmail.com>
2023-01-25 17:04:20 -0500

    First commit

---
+++ src/main/java/com/pragma/lambda/AsignarAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignarAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsignarAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/lambda/AsignarAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsignarAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
